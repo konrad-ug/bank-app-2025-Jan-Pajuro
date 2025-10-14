eb2681b21fdd2c340ebdcfaedc33051d473e221e
Jan Pajuro <jpajuro@INF.UG.EDU.PL>
2025-10-14 15:39:27 +0200

    feature2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -8,3 +8,9 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "12345678910"
+    def test_pesel_validation(self):
+        account_short = Account("John", "Doe", "0123456789")
+        assert account_short.pesel == "Invalid"
+        account_long = Account("Joe", "Dohn", "012345678901")
+        assert account_long.pesel == "Invalid"
+        account_not_digit = Account("John", "Doe", "abc34567890")
